bcc1bf8b1fb0b288cbc6d529f443a3af57ef50f5
ItAntoninR <antonin.riquart@ittalent.fr>
2024-12-18 09:52:14 +0100

    feat: add new column for client

--- src/App.vue
+++ src/App.vue
@@ -34,6 +34,7 @@ const signDataAndSend = async () => {
     const data = {
       name: 'BOUSNINA Achraf',
       email: 'bsachref@gmail.com',
+      semantics: 'test',
     }
 
     // Charger la clé privée et le kid
